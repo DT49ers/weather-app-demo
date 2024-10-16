bce73ce33e300e9237d8501c2b4b2cb2172467f4
Daniel Tovar <chicano878@gmail.com>
2024-10-15 22:42:07 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,56 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React, { useRef, useState } from "react";
+import "./App.css";
+
+import axios from "axios";
 
 function App() {
+  const [weatherInfo, setWeatherInfo] = useState(null);
+  const inputRef = useRef(null);
+
+  const fetchWeatherInfo = (e) => {
+    e.preventDefault();
+
+    const options = {
+      method: "GET",
+      url: "https://api.tomorrow.io/v4/weather/realtime",
+      params: {
+        location: inputRef.current.value, // Replace with your zipcode or city name
+        units: "imperial",
+        apikey: "cCntGe8w9ZA76OYa2PpQ9faaqTCLCOms",
+      },
+      headers: { accept: "application/json" },
+    };
+
+    axios
+      .request(options)
+      .then(function (response) {
+        setWeatherInfo(response.data);
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <h1>My weather app</h1>
+      <form>
+        <input ref={inputRef} type="text" placeholder="Zipcode...[23229 US]" />
+        <button onClick={fetchWeatherInfo} type="submit">
+          Show me the weather
+        </button>
+      </form>
+      <h2>{weatherInfo?.location.name}</h2>
     </div>
   );
 }
