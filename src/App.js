2353dd70f0fa645f086174ef42645ddffe6cafb0
Daniel Tovar <chicano878@gmail.com>
2024-10-16 23:53:45 -0400

    added some styling

--- src/App.js
+++ src/App.js
@@ -7,10 +7,20 @@ import moment from "moment/moment";
 function App() {
   const [weatherInfo, setWeatherInfo] = useState(null);
   const inputRef = useRef(null);
+  const [magicNumbers] = useState([12, 24, 36, 48, 60, 100]);
+  const [totalOfMagicNumbers, setTotalOfMagicNumbers] = useState(0);
 
   useEffect(() => {
     fetchWeatherInfo();
   }, []); // Focus on input when component mounts
+
+  useEffect(() => {
+    const total = magicNumbers.reduce(
+      (acc, magicNumber) => acc + magicNumber,
+      0
+    );
+    setTotalOfMagicNumbers(total);
+  }, [magicNumbers]); // Focus on magicNumbers when component updates
 
   const fetchWeatherInfo = (e) => {
     e?.preventDefault();
@@ -38,23 +48,34 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <h1>My weather app</h1>
-      <form>
-        <input ref={inputRef} type="text" placeholder="Zipcode...[23229 US]" />
-        <button onClick={fetchWeatherInfo} type="submit">
-          Show me the weather
-        </button>
-      </form>
-      <h2>{weatherInfo?.location.name}</h2>
-      <h3>
-        {weatherInfo &&
-          `Sunrise: ${moment(
-            weatherInfo?.timelines?.daily[1].values?.sunriseTime
-          ).format("LLL")}`}
+    <div className="app">
+      <div className="app__container">
+        <h1>My weather app</h1>
+        <form>
+          <input
+            ref={inputRef}
+            type="text"
+            placeholder="Zipcode...[23229 US]"
+          />
+          <button onClick={fetchWeatherInfo} type="submit">
+            Show me the weather
+          </button>
+        </form>
+        <h2>{weatherInfo?.location.name}</h2>
+        <h2>
+          {moment(weatherInfo?.timelines?.daily[1].time).format("LL")} Maximum
+          Temp: {weatherInfo?.timelines?.daily[1].values?.temperatureMax} ºF
+        </h2>
+        <h3>
+          {weatherInfo &&
+            `Sunrise: ${moment(
+              weatherInfo?.timelines?.daily[2].values?.sunriseTime
+            ).format("LLL")}`}
+        </h3>
 
-        {/* {weatherInfo && weatherInfo?.timelines?.daily[1].values?.sunriseTime} */}
-      </h3>
+        <h2>My magic numbers: {magicNumbers.join(", ")}</h2>
+        <h3>Total of magic numbers: {totalOfMagicNumbers}</h3>
+      </div>
     </div>
   );
 }
